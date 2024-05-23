909f962eb7cae4efb319840b447f4abb90ef6fcf
Natalia Semenova <nataliaberten@gmail.com>
2024-05-22 20:52:03 -0400

    new

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 
+
 const app = express();
 
 const { PORT = 3001 } = process.env;
--- models/user.js
+++ models/user.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    //select: true,
+    select: true,
     required: [true, "The avatar field is required."],
     validate: {
       validator(value) {
